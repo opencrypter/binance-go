2114ae165d4d5c8cf1773b1e6259b53ef4202dcd
Daniel <dan@dfu.io>
2018-06-25 23:29:27 +0200

    Added kline data (#7)

---
+++ klines.go
@@ -0,0 +1,121 @@
+package binance
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+const (
+	Interval1m  Interval = "1m"
+	Interval3m  Interval = "3m"
+	Interval5m  Interval = "5m"
+	Interval15m Interval = "15m"
+	Interval30m Interval = "30m"
+	Interval1h  Interval = "1h"
+	Interval2h  Interval = "2h"
+	Interval4h  Interval = "4h"
+	Interval8h  Interval = "8h"
+	Interval12h Interval = "12h"
+	Interval1d  Interval = "1d"
+	Interval3d  Interval = "3d"
+	Interval1w  Interval = "1w"
+	Interval1M  Interval = "1M"
+)
+
+type Interval string
+
+type KLine struct {
+	OpenTime            int
+	Open                float64
+	High                float64
+	Low                 float64
+	Close               float64
+	Volume              float64
+	CloseTime           int
+	QuoteAssetVolume    float64
+	TradesNumber        int
+	TakerBuyBaseVolume  float64
+	TakerBuyQuoteVolume float64
+}
+
+type kLinesQuery struct {
+	symbol    string
+	interval  Interval
+	limit     int
+	startTime int
+	endTime   int
+}
+
+func NewKLinesQuery(symbol string, interval Interval) *kLinesQuery {
+	return &kLinesQuery{symbol: symbol, interval: interval}
+}
+
+func (t *kLinesQuery) Limit(limit int) *kLinesQuery {
+	t.limit = limit
+	return t
+}
+func (t *kLinesQuery) StartTime(startTime int) *kLinesQuery {
+	t.startTime = startTime
+	return t
+}
+
+func (t *kLinesQuery) EndTime(endTime int) *kLinesQuery {
+	t.endTime = endTime
+	return t
+}
+
+func parseKLinesResponse(jsonContent []byte) ([]KLine, error) {
+	elements := make([][]interface{}, 0)
+	err := json.Unmarshal(jsonContent, &elements)
+
+	kLines := make([]KLine, 0)
+	for _, item := range elements {
+		open, _ := strconv.ParseFloat(item[1].(string), 64)
+		high, _ := strconv.ParseFloat(item[2].(string), 64)
+		low, _ := strconv.ParseFloat(item[3].(string), 64)
+		close, _ := strconv.ParseFloat(item[4].(string), 64)
+		volume, _ := strconv.ParseFloat(item[5].(string), 64)
+		quoteVolume, _ := strconv.ParseFloat(item[7].(string), 64)
+		takerBuyBaseVolume, _ := strconv.ParseFloat(item[9].(string), 64)
+		takerBuyQuoteVolume, _ := strconv.ParseFloat(item[10].(string), 64)
+
+		kLines = append(kLines, KLine{
+			OpenTime:            int(item[0].(float64)),
+			Open:                open,
+			High:                high,
+			Low:                 low,
+			Close:               close,
+			Volume:              volume,
+			CloseTime:           int(item[6].(float64)),
+			QuoteAssetVolume:    quoteVolume,
+			TradesNumber:        int(item[8].(float64)),
+			TakerBuyBaseVolume:  takerBuyBaseVolume,
+			TakerBuyQuoteVolume: takerBuyQuoteVolume,
+		})
+	}
+
+	return kLines, err
+}
+
+func (sdk *Sdk) KLines(query *kLinesQuery) ([]KLine, error) {
+	url := "/api/v1/klines" + "?symbol=" + query.symbol + "&interval=" + string(query.interval)
+
+	if query.limit > 0 {
+		url += "&limit=" + strconv.Itoa(query.limit)
+	}
+
+	if query.startTime > 0 {
+		url += "&startTime=" + strconv.Itoa(query.startTime)
+	}
+
+	if query.endTime > 0 {
+		url += "&endTime=" + strconv.Itoa(query.endTime)
+	}
+
+	responseContent, err := sdk.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseKLinesResponse(responseContent)
+}
---
+++ klines_test.go
@@ -0,0 +1,109 @@
+package binance
+
+import (
+	"errors"
+	"github.com/isd4n/binance-go-sdk/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestSdk_KLines(t *testing.T) {
+	t.Run("It should convert api response to a compressed KLine slice", func(t *testing.T) {
+		clientMock := &mocks.Client{}
+		sdk := Sdk{client: clientMock}
+
+		expected := validKLinesJson()
+
+		clientMock.On("Get", mock.MatchedBy(func(path string) bool {
+			return path == "/api/v1/klines?symbol=ETHBTC&interval=1m"
+		})).Return(expected, nil)
+
+		response, _ := sdk.KLines(NewKLinesQuery("ETHBTC", Interval1m))
+
+		assert.Equal(t, validKLinesResponse(), response)
+	})
+
+	t.Run("It should read optional parameters", func(t *testing.T) {
+		clientMock := &mocks.Client{}
+		sdk := Sdk{client: clientMock}
+
+		expected := validKLinesJson()
+
+		clientMock.On("Get", mock.MatchedBy(func(path string) bool {
+			return path == "/api/v1/klines?symbol=ETHBTC&interval=1m"+
+				"&limit=10&startTime=1498793709153&endTime=1498793709163"
+		})).Return(expected, nil)
+
+		query := NewKLinesQuery("ETHBTC", Interval1m).Limit(10).StartTime(1498793709153).EndTime(1498793709163)
+		response, _ := sdk.KLines(query)
+
+		assert.Equal(t, validKLinesResponse(), response)
+	})
+
+	t.Run("It should return error when api fails", func(t *testing.T) {
+		clientMock := &mocks.Client{}
+		sdk := Sdk{client: clientMock}
+
+		expectedError := errors.New("error")
+
+		clientMock.On("Get", mock.MatchedBy(func(path string) bool {
+			return path == "/api/v1/klines?symbol=ETHBTC&interval=1m"
+		})).Return(nil, expectedError)
+
+		_, err := sdk.KLines(NewKLinesQuery("ETHBTC", Interval1m))
+
+		assert.Equal(t, expectedError, err)
+	})
+
+	t.Run("It should return error when response cannot be mapped", func(t *testing.T) {
+		clientMock := &mocks.Client{}
+		sdk := Sdk{client: clientMock}
+
+		clientMock.On("Get", mock.MatchedBy(func(path string) bool {
+			return path == "/api/v1/klines?symbol=ETHBTC&interval=1m"
+		})).Return(invalidKLinesJson(), nil)
+
+		_, err := sdk.KLines(NewKLinesQuery("ETHBTC", Interval1m))
+
+		assert.Error(t, err)
+	})
+}
+
+func invalidKLinesJson() []byte {
+	return []byte(`<h1>Page Not available</h1>`)
+}
+func validKLinesJson() []byte {
+	return []byte(`[
+		[	
+    		1499040000000,      
+    		"0.01634790",       
+    		"0.80000000",       
+    		"0.01575800",       
+    		"0.01577100",       
+    		"148976.11427815",  
+    		1499644799999,      
+    		"2434.19055334",    
+    		308,                
+    		"1756.87402397",    
+    		"28.46694368",      
+    		"17928899.62484339" 
+  		]
+	]`)
+}
+
+func validKLinesResponse() []KLine {
+	return []KLine{{
+		OpenTime:            1499040000000,
+		Open:                0.01634790,
+		High:                0.8,
+		Low:                 0.015758,
+		Close:               0.015771,
+		Volume:              148976.11427815,
+		CloseTime:           1499644799999,
+		QuoteAssetVolume:    2434.19055334,
+		TradesNumber:        308,
+		TakerBuyBaseVolume:  1756.87402397,
+		TakerBuyQuoteVolume: 28.46694368,
+	}}
+}
